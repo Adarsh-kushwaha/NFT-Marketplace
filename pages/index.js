aeaa14f4d7f2b94d101b5fe8b587e0ba99ea0e12
adarshkushwaha <adarshkushwaha296@gmail.com>
2022-05-11 16:14:10 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div className='text-2xl bg-blue-500 p-4 items-center text-center text-white'>Welcome to NFT Marketplace</div>
+  )
+}
+
+export default index
