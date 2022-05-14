6c6dc138dedcae58b3fc6e3b62459acbcd2f430a
Chhaya Bisen <chhayabisen141@gmail.com>
2022-05-15 00:07:16 +0530

    grid ui

---
+++ grid/grid.js
@@ -0,0 +1,86 @@
+import React from 'react';
+
+export default function grid() {
+  return (
+    <>
+      <div
+        id="main"
+        className="inline-grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4 w-full mt-6 pr-8 pl-8"
+      >
+        <div className="p-6 bg-white  rounded-lg ml-4 mr-4">
+          <div className="overflow-hidden">
+            <img
+              className="object-cover w-full h-48"
+              src="https://images.unsplash.com/photo-1516205651411-aef33a44f7c2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=420&q=80"
+              alt="Flower and sky"
+            />
+          </div>
+          <h3 className="mt-6 leading-normal text-gray-400 group-hover:text-purple-400  line-clamp-3 transition translation-all duration-200 ease-in-out">
+            Lorem Ipsum is simply dummy text
+          </h3>
+          <h3>Lorem Ipsum g</h3>
+          <h3 className="text-blue-400">Lorem Ipsum g</h3>
+        </div>
+        <div className="p-6 bg-white  rounded-lg ml-4 mr-4 ">
+          <div className="overflow-hidden">
+            <img
+              className="object-cover w-full h-48"
+              src="https://images.unsplash.com/photo-1516205651411-aef33a44f7c2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=420&q=80"
+              alt="Flower and sky"
+            />
+          </div>
+          <h3 className="mt-6 leading-normal text-gray-400 group-hover:text-purple-400  line-clamp-3 transition translation-all duration-200 ease-in-out">
+            Lorem Ipsum is simply dummy text
+          </h3>
+          <h3>Lorem Ipsum g</h3>
+          <h3 className="text-blue-400">Lorem Ipsum g</h3>
+        </div>
+
+        <div className="p-6 bg-white  rounded-lg ml-4 mr-4 ">
+          <div className="overflow-hidden">
+            <img
+              className="object-cover w-full h-48"
+              src="https://images.unsplash.com/photo-1516205651411-aef33a44f7c2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=420&q=80"
+              alt="Flower and sky"
+            />
+          </div>
+          <h3 className="mt-6 leading-normal text-gray-400 group-hover:text-purple-400  line-clamp-3 transition translation-all duration-200 ease-in-out">
+            Lorem Ipsum is simply dummy text
+          </h3>
+          <h3>Lorem Ipsum g</h3>
+          <h3 className="text-blue-400">Lorem Ipsum g</h3>
+        </div>
+
+        <div className="p-6 bg-white  rounded-lg ml-4 mr-4 ">
+          <div className="overflow-hidden">
+            <img
+              className="object-cover w-full h-48"
+              src="https://images.unsplash.com/photo-1516205651411-aef33a44f7c2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=420&q=80"
+              alt="Flower and sky"
+            />
+          </div>
+          <h3 className="mt-6 leading-normal text-gray-400 group-hover:text-purple-400  line-clamp-3 transition translation-all duration-200 ease-in-out">
+            Lorem Ipsum is simply dummy text
+          </h3>
+          <h3>Lorem Ipsum g</h3>
+          <h3 className="text-blue-400">Lorem Ipsum g</h3>
+        </div>
+
+        <div className="p-6 bg-white  rounded-lg ml-4 mr-4">
+          <div className="overflow-hidden">
+            <img
+              className="object-cover w-full h-48"
+              src="https://images.unsplash.com/photo-1516205651411-aef33a44f7c2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=420&q=80"
+              alt="Flower and sky"
+            />
+          </div>
+          <h3 className="mt-6 leading-normal text-gray-400 group-hover:text-purple-400  line-clamp-3 transition translation-all duration-200 ease-in-out">
+            Lorem Ipsum is simply dummy text
+          </h3>
+          <h3>Lorem Ipsum g</h3>
+          <h3 className="text-blue-400">Lorem Ipsum g</h3>
+        </div>
+      </div>
+    </>
+  );
+}
